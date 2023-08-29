8faf55833c4ab062e50aa1b1b8194b833432b37c
mahasultan <ajabor@andrew.cmu.edu>
2023-08-29 20:03:26 +0300

    Fixing home.ts

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,64 +1,96 @@
-'use strict';
-
-const url = require('url');
-
-const plugins = require('../plugins');
-const meta = require('../meta');
-const user = require('../user');
-
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pluginHook = exports.rewrite = void 0;
+const url = __importStar(require("url"));
+const plugins = __importStar(require("../plugins"));
+const meta = __importStar(require("../meta"));
+const user = __importStar(require("../user"));
 function adminHomePageRoute() {
-    return ((meta.config.homePageRoute === 'custom' ? meta.config.homePageCustom : meta.config.homePageRoute) || 'categories').replace(/^\//, '');
+    const { config } = meta;
+    const { homePageRoute } = config;
+    const { homePageCustom } = config;
+    const route = (homePageRoute === 'custom' ? homePageCustom : homePageRoute) || 'categories';
+    return route.replace(/^\//, '');
 }
-
-async function getUserHomeRoute(uid) {
-    const settings = await user.getSettings(uid);
-    let route = adminHomePageRoute();
-
-    if (settings.homePageRoute !== 'undefined' && settings.homePageRoute !== 'none') {
-        route = (settings.homePageRoute || route).replace(/^\/+/, '');
-    }
-
-    return route;
+function getUserHomeRoute(uid) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const settings = yield user.getSettings(uid);
+        let route = adminHomePageRoute();
+        if (settings.homePageRoute !== 'undefined' && settings.homePageRoute !== 'none') {
+            route = (settings.homePageRoute || route).replace(/^\/+/, '');
+        }
+        return route;
+    });
 }
-
-async function rewrite(req, res, next) {
-    if (req.path !== '/' && req.path !== '/api/' && req.path !== '/api') {
-        return next();
-    }
-    let route = adminHomePageRoute();
-    if (meta.config.allowUserHomePage) {
-        route = await getUserHomeRoute(req.uid, next);
-    }
-
-    let parsedUrl;
-    try {
-        parsedUrl = url.parse(route, true);
-    } catch (err) {
-        return next(err);
-    }
-
-    const { pathname } = parsedUrl;
-    const hook = `action:homepage.get:${pathname}`;
-    if (!plugins.hooks.hasListeners(hook)) {
-        req.url = req.path + (!req.path.endsWith('/') ? '/' : '') + pathname;
-    } else {
-        res.locals.homePageRoute = pathname;
-    }
-    req.query = Object.assign(parsedUrl.query, req.query);
-
-    next();
+function rewrite(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (req.path !== '/' && req.path !== '/api/' && req.path !== '/api') {
+            return next();
+        }
+        let route = adminHomePageRoute();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        if (meta.config.allowUserHomePage) {
+            route = yield getUserHomeRoute(req.uid);
+        }
+        let parsedUrl;
+        try {
+            parsedUrl = url.parse(route, true);
+        }
+        catch (err) {
+            return next(err);
+        }
+        const { pathname } = parsedUrl;
+        const hook = `action:homepage.get:${pathname}`;
+        if (!plugins.hooks.hasListeners(hook)) {
+            req.url = req.path + (!req.path.endsWith('/') ? '/' : '') + pathname;
+        }
+        else {
+            res.locals.homePageRoute = pathname;
+        }
+        req.query = Object.assign(parsedUrl.query, req.query);
+        next();
+    });
 }
-
 exports.rewrite = rewrite;
-
 function pluginHook(req, res, next) {
     const hook = `action:homepage.get:${res.locals.homePageRoute}`;
-
     plugins.hooks.fire(hook, {
         req: req,
         res: res,
         next: next,
     });
 }
-
 exports.pluginHook = pluginHook;
---
+++ src/controllers/home.ts
@@ -0,0 +1,78 @@
+import * as express from 'express';
+import * as url from 'url';
+import * as plugins from '../plugins';
+import * as meta from '../meta';
+import * as user from '../user';
+
+declare module 'express' {
+    interface Request {
+        uid: string;
+    }
+}
+
+
+function adminHomePageRoute(): string {
+    const { config } = meta;
+    const { homePageRoute } = config;
+    const { homePageCustom } = config;
+
+    const route = (homePageRoute === 'custom' ? homePageCustom : homePageRoute) || 'categories';
+
+    return route.replace(/^\//, '');
+}
+
+async function getUserHomeRoute(uid: string): Promise<string> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const settings = await user.getSettings(uid);
+    let route = adminHomePageRoute();
+
+    if (settings.homePageRoute !== 'undefined' && settings.homePageRoute !== 'none') {
+        route = (settings.homePageRoute || route).replace(/^\/+/, '');
+    }
+
+    return route;
+}
+
+async function rewrite(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
+    if (req.path !== '/' && req.path !== '/api/' && req.path !== '/api') {
+        return next();
+    }
+    let route = adminHomePageRoute();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (meta.config.allowUserHomePage) {
+        route = await getUserHomeRoute(req.uid);
+    }
+
+    let parsedUrl: url.UrlWithParsedQuery;
+    try {
+        parsedUrl = url.parse(route, true);
+    } catch (err) {
+        return next(err);
+    }
+
+    const { pathname } = parsedUrl;
+    const hook = `action:homepage.get:${pathname}`;
+    if (!plugins.hooks.hasListeners(hook)) {
+        req.url = req.path + (!req.path.endsWith('/') ? '/' : '') + pathname;
+    } else {
+        res.locals.homePageRoute = pathname;
+    }
+    req.query = Object.assign(parsedUrl.query, req.query);
+
+    next();
+}
+
+export { rewrite };
+
+function pluginHook(req: express.Request, res: express.Response, next: express.NextFunction): void {
+    const hook = `action:homepage.get:${res.locals.homePageRoute}`;
+
+    plugins.hooks.fire(hook, {
+        req: req,
+        res: res,
+        next: next,
+    });
+}
+
+
+export { pluginHook };
